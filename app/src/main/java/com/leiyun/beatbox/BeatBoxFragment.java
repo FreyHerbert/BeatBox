c8620711525d22144d6584274e8a5abb1389e2ee
FreyHerbert <leiyun951102@gmail.com>
2016-11-21 19:08:10 +0800

    完成布局

---
+++ app/src/main/java/com/leiyun/beatbox/BeatBoxFragment.java
@@ -0,0 +1,10 @@
+package com.leiyun.beatbox;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by LeiYun on 2016/11/21 0021.
+ */
+
+public class BeatBoxFragment extends Fragment {
+}
---
+++ app/src/main/java/com/leiyun/beatbox/Sound.java
@@ -0,0 +1,8 @@
+package com.leiyun.beatbox;
+
+/**
+ * Created by LeiYun on 2016/11/21 0021.
+ */
+
+public class Sound {
+}
