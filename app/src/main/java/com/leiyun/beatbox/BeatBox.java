43fdeec81658b359339f17acd25883f8bd3a6e97
FreyHerbert <leiyun951102@gmail.com>
2016-11-21 22:43:56 +0800

    完成音频播放

--- app/src/main/java/com/leiyun/beatbox/BeatBox.java
+++ app/src/main/java/com/leiyun/beatbox/BeatBox.java
@@ -1,8 +1,93 @@
 package com.leiyun.beatbox;
 
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by LeiYun on 2016/11/21 0021.
+ * 一个BeatBox的资源管理类
  */
 
 public class BeatBox {
+    private static final String TAG = "BeatBox";
+
+    private static final String SOUNDS_FOLDER = "sample_sounds";
+    private static final int MAX_SOUND = 5;
+
+    private AssetManager mAssets;
+    private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
+
+    public BeatBox(Context context) {
+        mAssets = context.getAssets();
+        // 这个构造函数已经不建议使用了
+        // 但是为了兼容性我还是使用了这个构造方法
+        mSoundPool = new SoundPool(MAX_SOUND, AudioManager.STREAM_MUSIC, 0);
+        loadSounds();
+    }
+
+    /**
+     * 关联音频文件
+     */
+    private void loadSounds() {
+        String[] soundNames;
+        try{
+            soundNames = mAssets.list(SOUNDS_FOLDER);
+            Log.i(TAG, "Found " + soundNames.length + " sounds");
+        } catch (IOException e) {
+            Log.e(TAG, "Could not list assets", e);
+            return;
+        }
+
+        for (String filename :
+                soundNames) {
+            try {
+                String assetPath = SOUNDS_FOLDER + "/" + filename;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            } catch (IOException e) {
+                Log.e(TAG, "Could not load sound" + filename, e);
+            }
+        }
+    }
+
+    /**
+     * SoundPool预加载音频
+     * @param sound
+     * @throws IOException
+     */
+    private void load(Sound sound) throws IOException {
+        AssetFileDescriptor afd = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(afd, 1);
+        sound.setSoundId(soundId);
+    }
+
+    public List<Sound> getSounds() {
+        return mSounds;
+    }
+
+    /**
+     * 播放音频
+     * @param sound
+     */
+    public void play(Sound sound) {
+        Integer soundId = sound.getSoundId();
+        if (soundId == null) {
+            return;
+        }
+        mSoundPool.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    public void release() {
+        mSoundPool.release();
+    }
 }
